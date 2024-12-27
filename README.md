88da5d1e44e86ef4cebd8d6d08689942fd7485fc
migliorellI <miglenti@gmail.com>
2024-12-27 02:58:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Instagram Video Progress Bar
+
+This extension adds a progress bar (basically toggles on the **video** tag _controls_ property) to all Instagram videos. This includes in-feed posts and reels, the reels page and all other videos.
+
+## Installation
+
+Since I don't want to pay to publish this extension, you will need to clone this repo and use 'Load Unpacked Extension'.
+Read more on [Development Basics](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+
+## Contributing
+
+1. Fork the project.
+2. Create a new branch:
+
+   ```bash
+   git checkout -b feature/new-feature
+   ```
+
+3. Make your changes and commit them:
+
+   ```bash
+   git commit -m "Add new feature"
+   ```
+
+4. Push your changes:
+
+   ```bash
+   git push origin feature/new-feature
+   ```
+
+5. Open a Pull Request.
+
+## Icon
+
+Icon from [Icons8](https://icons8.com.br/icon/t294OHA3a4ko/circled-play)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Contact
+
+- Author: **Migliorelli**
+- Email: [miglenten@gmail.com](mailto:miglenten@gmail.com)
---
+++ content.js
@@ -0,0 +1,159 @@
+const FEED_VIDEOS_SELECTORS = "main[role=main] article video";
+const REELS_VIDEOS_SELECTORS = "main[role=main] video";
+const POST_VIDEOS_SELECTOR = "main[role=main] video";
+const VIDEO_INFOS_SELECTORS =
+  "div > div > div > div > div:nth-of-type(2) > div";
+const VIDEO_SOUND_BTN_SELECTORS = "div[role=button] :has(svg) div";
+
+class InstagramVideoProgressBar {
+  observer = null;
+
+  initialize() {
+    console.log("Instagram Video Progress Bar intialized!");
+    this.handleUrlChanges();
+    this.initializeObserver();
+  }
+
+  handleUrlChanges() {
+    let lastUrl = location.href;
+
+    new MutationObserver(() => {
+      const url = location.href;
+      if (url !== lastUrl) {
+        lastUrl = url;
+        console.log("URL changed:", url);
+        this.initializeObserver();
+      }
+    }).observe(document, { subtree: true, childList: true });
+  }
+
+  initializeObserver() {
+    this.observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length) {
+          this.handleNewContent();
+        }
+      });
+    });
+
+    const observerConfig = {
+      childList: true,
+      subtree: true,
+    };
+
+    this.waitForContent().then((container) => {
+      this.observer.observe(container, observerConfig);
+      this.handleNewContent();
+    });
+  }
+
+  async waitForContent() {
+    return new Promise((resolve) => {
+      const checkContent = () => {
+        const container = document.querySelector("main[role=main]");
+        if (container) {
+          resolve(container);
+        } else {
+          setTimeout(checkContent, 1000);
+        }
+      };
+      checkContent();
+    });
+  }
+
+  handleNewContent() {
+    if (this.isReelsPage()) {
+      this.handleReels();
+    } else if (this.isReelPost()) {
+      this.handleReelPostPage();
+    } else if (this.isPostPage()) {
+      this.handlePost();
+    } else {
+      this.handleFeed();
+    }
+  }
+
+  isReelsPage() {
+    return document.location.pathname.includes("/reels");
+  }
+
+  isReelPost() {
+    return document.location.pathname.includes("/reel");
+  }
+
+  isPostPage() {
+    return document.location.pathname.includes("/p");
+  }
+
+  addVideoProgressBar(videoEl, deleteOverlay = true) {
+    if (videoEl && !videoEl.hasAttribute("data-processed")) {
+      videoEl.controls = true;
+
+      if (deleteOverlay) {
+        const videoOverlay = videoEl.nextElementSibling;
+        if (videoOverlay) videoOverlay.remove();
+      }
+
+      videoEl.setAttribute("data-processed", "true");
+    }
+  }
+
+  handleNewVideos(selectors) {
+    const elements = document.querySelectorAll(selectors);
+    elements.forEach((videoEl) => this.addVideoProgressBar(videoEl));
+  }
+
+  moveReelInfos(videoEl) {
+    if (videoEl.hasAttribute("data-moved")) {
+      return;
+    }
+
+    const overlay = videoEl.nextElementSibling;
+    const soundBtn = overlay.querySelector(VIDEO_SOUND_BTN_SELECTORS);
+    const infos = overlay.querySelector(VIDEO_INFOS_SELECTORS);
+
+    const parent = videoEl.parentElement;
+    parent.style.position = "relative";
+
+    parent.appendChild(soundBtn);
+    parent.appendChild(infos);
+
+    soundBtn.style.position = "absolute";
+    soundBtn.style.right = "16px";
+    soundBtn.style.top = "16px";
+
+    infos.style.position = "absolute";
+    infos.style.left = "16px";
+    infos.style.bottom = "80px";
+    infos.style.width = `${parent.clientWidth - 32}px`;
+
+    overlay.remove();
+    videoEl.setAttribute("data-moved", "true");
+  }
+
+  handleReels() {
+    const videos = document.querySelectorAll(REELS_VIDEOS_SELECTORS);
+
+    videos.forEach((videoEl) => {
+      this.addVideoProgressBar(videoEl, false);
+      this.moveReelInfos(videoEl);
+    });
+  }
+
+  handleFeed() {
+    this.handleNewVideos(FEED_VIDEOS_SELECTORS);
+  }
+
+  handlePost() {
+    this.handleNewVideos(POST_VIDEOS_SELECTOR);
+  }
+
+  handleReelPostPage() {
+    this.handleNewVideos(POST_VIDEOS_SELECTOR);
+  }
+}
+
+window.addEventListener("load", () => {
+  const IVP = new InstagramVideoProgressBar();
+  IVP.initialize();
+});
